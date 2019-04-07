1a9f4d0c52362e8ac66ef66b29a76b88af8a9fac
gbarrbrown <gbb7899@gmail.com>
2019-04-07 15:21:14 +1200

    Use the Sqrt math fn with Println

---
+++ sqrt.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var x float64 = 16  
+	var y float64 = 25
+	fmt.Println("sqrt of", x, ":", math.Sqrt(x))
+	fmt.Println("sqrt of", y, ":", math.Sqrt(y))
+
+}
